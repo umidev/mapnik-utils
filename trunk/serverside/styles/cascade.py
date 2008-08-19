eed4a288af7e3f68e58b300d6c7f91d28f921b8b
migurski <migurski@dbea3d40-3153-0410-b551-57805770c6e6>
2008-08-19 05:34:32 +0000

    Starting to think about cascade order and constraints on selectors
    git-svn-id: http://mapnik-utils.googlecode.com/svn@40 dbea3d40-3153-0410-b551-57805770c6e6

--- trunk/serverside/styles/cascade.py
+++ trunk/serverside/styles/cascade.py
@@ -3,7 +3,72 @@ import pprint
 import simplejson
 import cssutils.tokenize2
 
-def parse_rules(s):
+class Selector:
+    def __init__(self, *atoms):
+    
+        if len(atoms) > 2:
+            raise Exception('Only two-atom selectors are supported for Mapnik styles')
+
+        if len(atoms) == 2 and atoms[1].hasTests():
+            raise Exception('Only the first atom in a selector may have attributes in Mapnik styles')
+
+        if len(atoms) == 2 and atoms[1].isID():
+            raise Exception('Only the first atom in a selector may be an ID in Mapnik styles')
+    
+        if len(atoms) == 2 and atoms[1].isClass():
+            raise Exception('Only the first atom in a selector may be a class in Mapnik styles')
+    
+        self.atoms = atoms[:]
+
+    def specificity(self):
+        """ Loosely based on http://www.w3.org/TR/REC-CSS2/cascade.html#specificity
+        """
+        ids = 0
+        elements = len(self.atoms)
+        tests = sum(len(a.tests) for a in self.atoms)
+        
+        return '%(ids)04d %(elements)04d %(tests)04d' % locals()
+
+    def __repr__(self):
+        return ' '.join(repr(a) for a in self.atoms)
+
+class SelectorAtom:
+    def __init__(self, name):
+        self.name = name
+        self.tests = []
+
+    def addTest(self, test):
+        self.tests.append(test)
+
+    def hasTests(self):
+        return bool(len(self.tests))
+    
+    def isID(self):
+        return self.name.startswith('#')
+    
+    def isClass(self):
+        return self.name.startswith('.')
+    
+    def __repr__(self):
+        return self.name + ''.join(repr(t) for t in self.tests)
+
+class SelectorAttributeTest:
+    def __init__(self, arg1, op, arg2):
+        self.op = op
+        self.arg1 = arg1
+        self.arg2 = arg2
+
+    def __repr__(self):
+        return '[%(arg1)s%(op)s%(arg2)s]' % self.__dict__
+
+class Property:
+    def __init__(self, name):
+        self.name = name
+
+    def __repr__(self):
+        return self.name
+
+def parse_rulesets(s):
     """
     """
     in_selectors = False
@@ -11,7 +76,7 @@ def parse_rules(s):
     in_declaration = False # implies in_block
     in_property = False # implies in_declaration
     
-    rules = []
+    rulesets = []
     tokens = cssutils.tokenize2.Tokenizer().tokenize(s)
     
     for token in tokens:
@@ -21,33 +86,33 @@ def parse_rules(s):
             if not in_selectors and not in_block:
                 if nname == 'IDENT' or (nname == 'CHAR' and value != '{'):
                     # beginning of a 
-                    rules.append({'selectors': [[(nname, value)]], 'declarations': [], 'position': (line, col)})
+                    rulesets.append({'selectors': [[(nname, value)]], 'declarations': []})
                     in_selectors = True
                     
             elif in_selectors and not in_block:
-                rule = rules[-1]
+                ruleset = rulesets[-1]
             
                 if (nname == 'CHAR' and value == '{'):
-                    # open curly-brace means we're on to the actual rules
-                    rule['selectors'][-1] = trim_extra(rule['selectors'][-1])
+                    # open curly-brace means we're on to the actual rule sets
+                    ruleset['selectors'][-1] = postprocess_selector(ruleset['selectors'][-1])
                     in_selectors = False
                     in_block = True
     
                 elif (nname == 'CHAR' and value == ','):
                     # comma means there's a break between selectors
-                    rule['selectors'][-1] = trim_extra(rule['selectors'][-1])
-                    rule['selectors'].append([])
+                    ruleset['selectors'][-1] = postprocess_selector(ruleset['selectors'][-1])
+                    ruleset['selectors'].append([])
     
                 elif nname not in ('COMMENT'):
                     # we're just in a selector is all
-                    rule['selectors'][-1].append((nname, value))
+                    ruleset['selectors'][-1].append((nname, value))
     
             elif in_block and not in_declaration:
-                rule = rules[-1]
+                ruleset = rulesets[-1]
             
                 if nname == 'IDENT':
                     # right at the start of a declaration
-                    rule['declarations'].append({'property': [(nname, value)], 'value': []})
+                    ruleset['declarations'].append({'property': [(nname, value)], 'value': [], 'position': (line, col)})
                     in_declaration = True
                     in_property = True
                     
@@ -56,7 +121,7 @@ def parse_rules(s):
                     in_block = False
     
             elif in_declaration and in_property:
-                declaration = rules[-1]['declarations'][-1]
+                declaration = rulesets[-1]['declarations'][-1]
             
                 if nname == 'CHAR' and value == ':':
                     # end of property
@@ -68,11 +133,11 @@ def parse_rules(s):
                     declaration['property'].append((nname, value))
     
             elif in_declaration and not in_property:
-                declaration = rules[-1]['declarations'][-1]
+                declaration = rulesets[-1]['declarations'][-1]
             
                 if nname == 'CHAR' and value == ';':
                     # end of declaration
-                    declaration['value'] = trim_extra(declaration['value'])
+                    declaration['value'] = postprocess_value(declaration['value'])
                     in_declaration = False
     
                 elif nname not in ('COMMENT'):
@@ -83,7 +148,7 @@ def parse_rules(s):
             print >> sys.stderr, 'Exception at line %(line)d, column %(col)d' % locals()
             raise
 
-    return rules
+    return rulesets
 
 def trim_extra(tokens):
     """
@@ -96,6 +161,51 @@ def trim_extra(tokens):
         
     return tokens
 
+def postprocess_selector(tokens):
+    """
+    """
+    tokens = (token for token in trim_extra(tokens))
+    
+    parts = []
+    
+    in_attribute = False
+    
+    for token in tokens:
+        nname, value = token
+        
+        if not in_attribute:
+            if nname == 'CHAR' and value in ('.', '#'):
+                next_nname, next_value = tokens.next()
+                
+                if next_nname == 'IDENT':
+                    parts.append(SelectorAtom(value + next_value))
+                
+            elif nname == 'IDENT':
+                parts.append(SelectorAtom(value))
+
+            elif (nname == 'CHAR' and value == '*'):
+                parts.append(SelectorAtom(value))
+
+            elif nname == 'CHAR' and value == '[':
+                parts.append([])
+                in_attribute = True
+                
+        elif in_attribute:
+            if nname == 'IDENT':
+                parts[-1].append(value)
+                
+            elif nname == 'CHAR' and value in ('<', '=', '>'):
+                parts[-1].append(value)
+
+            if nname == 'CHAR' and value == ']':
+                parts[-2].addTest(SelectorAttributeTest(*parts[-1][:3]))
+                parts = parts[:-1]
+                in_attribute = False
+    
+    selector = Selector(*parts)
+    
+    return selector
+
 def postprocess_property(tokens):
     """
     """
@@ -107,12 +217,19 @@ def postprocess_property(tokens):
     if tokens[0][0] != 'IDENT':
         raise Exception('Incorrect type of token in property: ' + repr(tokens))
     
-    return tokens[0][1]
+    return Property(tokens[0][1])
+
+def postprocess_value(tokens):
+    """
+    """
+    tokens = trim_extra(tokens)
+    
+    return tokens
 
 if __name__ == '__main__':
 
     s = """
-    .foo .bar[baz>quuz],
+    .foo[baz>quuz] bar,
     *
     {
         color: red;
@@ -121,9 +238,20 @@ if __name__ == '__main__':
         background: url(http://example.com);
     }
     
-    * { this: that; }
+    * { this: that !important; }
     """
     
-    rules = parse_rules(s)
+    rulesets = parse_rulesets(s)
+    
+    rules = []
+    
+    for ruleset in rulesets:
+        for declaration in ruleset['declarations']:
+            for selector in ruleset['selectors']:
+                rules.append({'selector': selector, 'property': declaration['property'], 'value': declaration['value'], 'position': declaration['position']})
 
-    pprint.PrettyPrinter(indent=4).pprint(rules)
+    # sort by a css-like method
+    rules.sort(key=lambda r: (r['selector'].specificity(), r['position'][0], r['position'][1]))
+
+    #pprint.PrettyPrinter(indent=2).pprint(rulesets)
+    pprint.PrettyPrinter(indent=2).pprint(rules)
