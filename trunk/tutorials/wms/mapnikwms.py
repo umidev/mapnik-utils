c87a71253178fe5c6c914cf50e7ce4d7400fa8c6
dane.springmeyer <dane.springmeyer@dbea3d40-3153-0410-b551-57805770c6e6>
2008-08-28 22:33:50 +0000

    Moved wms and tilecache configurations example to tutorials folder
    git-svn-id: http://mapnik-utils.googlecode.com/svn@102 dbea3d40-3153-0410-b551-57805770c6e6

---
+++ trunk/tutorials/wms/mapnikwms.py
@@ -0,0 +1,15 @@
+#!/usr/bin/env python
+
+# http://blog.brandonking.net//2008/01/django-app-modfcgid-apache-2-setup-on.html/
+# http://blog.cleverelephant.ca/2008/05/fastcgi-on-osx-leopard.html
+
+import sys
+sys.path.append('/Users/spring/projects/mapnik-utils/trunk/serverside/wms/')
+
+from mapnik.ogcserver.cgiserver import Handler
+from jon import fcgi
+
+class OGCServerHandler(Handler):
+    configpath = '/Users/spring/projects/mapnik-utils/trunk/serverside/wms/ogcserver.conf'
+
+fcgi.Server({fcgi.FCGI_RESPONDER: OGCServerHandler}).run()
