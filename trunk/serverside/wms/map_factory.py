d772781283b2ff7aa4b5e271974fb1ceb7a5e773
dane.springmeyer <dane.springmeyer@dbea3d40-3153-0410-b551-57805770c6e6>
2008-08-28 21:13:51 +0000

    Adding WMS reprojection example
    git-svn-id: http://mapnik-utils.googlecode.com/svn@99 dbea3d40-3153-0410-b551-57805770c6e6

--- trunk/serverside/wms/map_factory.py
+++ trunk/serverside/wms/map_factory.py
@@ -1,7 +1,15 @@
 from mapnik.ogcserver.WMS import BaseWMSFactory
 from mapnik import Shapefile, Layer, Style, Rule, Color, PolygonSymbolizer, LineSymbolizer
 
-SHAPEFILE = '/Users/spring/projects/mapnik-utils/trunk/serverside/wms/world_borders'
+SHAPEFILE = '/Users/spring/projects/mapnik-utils/trunk/sample_data/world_borders'
+PROJ4_STRING = '+init=epsg:4326'
+
+# Switch to these settings to used data in mercator projection from: http://tile.openstreetmap.org/world_boundaries-spherical.tgz 
+#PROJ4_STRING = '+init=epsg:3395'
+#SHAPEFILE = '../../world_boundaries_m'
+
+# Example query string for reprojected data:
+# http://localhost/cgi-bin/wms/mapnikwms.py?LAYERS=world&SERVICE=WMS&VERSION=1.1.1&REQUEST=GetMap&STYLES=&EXCEPTIONS=application%2Fvnd.ogc.se_inimage&FORMAT=image%2Fpng&SRS=EPSG%3A3395&BBOX=-20037400.000000,-19929239.110000,%2020037400.000000,18375854.709643&WIDTH=256&HEIGHT=256
 
 class WMSFactory(BaseWMSFactory):
   def __init__(self):
@@ -12,11 +20,11 @@ class WMSFactory(BaseWMSFactory):
     rl.symbols.append(LineSymbolizer(Color('steelblue'),.1))
     sty.rules.append( rl )
     self.register_style('s', sty)
-    lyr = Layer('world','+init=epsg:4326')
+    lyr = Layer('world',PROJ4_STRING)
     lyr.title = 'World Borders'
     lyr.abstract = 'World Test'
     lyr.queryable = True
     lyr.datasource = Shapefile(file=SHAPEFILE)
-    lyr.styles.append('s')
+    #lyr.styles.append('s')
     self.register_layer(lyr,'s',('s',))
     self.finalize()
