4f15ddeabb4458ca53f1a49bd63a8a3e3d41fd2f
dane.springmeyer <dane.springmeyer@dbea3d40-3153-0410-b551-57805770c6e6>
2008-08-29 07:30:48 +0000

    small tweaks
    git-svn-id: http://mapnik-utils.googlecode.com/svn@111 dbea3d40-3153-0410-b551-57805770c6e6

--- trunk/tutorials/google_mercator_projection/world_mercator.py
+++ trunk/tutorials/google_mercator_projection/world_mercator.py
@@ -3,7 +3,7 @@
 from mapnik import *
 mapfile = 'mapfile.xml'
 map_output = 'map/spherical_mercator.png'
-m = Map(1200, 700)
+m = Map(600, 400)
 load_map(m, mapfile)
 spherical_mercator = '+proj=merc +a=6378137 +b=6378137 +lat_ts=0.0 +lon_0=0.0 +x_0=0.0 +y_0=0 +k=1.0 +units=m +nadgrids=@null +no_defs +over'
 
@@ -12,7 +12,7 @@ m.srs = spherical_mercator
 
 # Set a bbox by reprojection Long/Lat coordinates
 p = Projection(spherical_mercator)
-lower_left_xy = p.forward(Coord(-180.0, -60.0))
+lower_left_xy = p.forward(Coord(-180.0, -45.0))
 upper_right_xy = p.forward(Coord(180.0, 80.0))
 bbox = Envelope(lower_left_xy,upper_right_xy)
 
