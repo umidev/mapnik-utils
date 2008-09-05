716a4cd589970bc98ca1c311d889ce34751583bb
dane.springmeyer <dane.springmeyer@dbea3d40-3153-0410-b551-57805770c6e6>
2008-09-05 01:58:44 +0000

    updated with better approach to using agg renderer with opacity and a few alpha examples
    git-svn-id: http://mapnik-utils.googlecode.com/svn@136 dbea3d40-3153-0410-b551-57805770c6e6

--- trunk/tutorials/hello_world/mapserver_comparison/mapfile_config/shp2img.sh
+++ trunk/tutorials/hello_world/mapserver_comparison/mapfile_config/shp2img.sh
@@ -1,1 +1,2 @@
-shp2img -m world.map -o map/hello_world_with_mapserver_mapfile.png
+shp2img -m world.map -l world -o map/hello_world_with_mapserver_mapfile.png
+shp2img -m world.map -l world_opacity -o map/hello_world_with_mapserver_mapfile_opacity.png
