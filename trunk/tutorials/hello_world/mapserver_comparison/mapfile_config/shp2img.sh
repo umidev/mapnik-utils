e0df93f60601dcb08a3baa3d44000712ee57365b
dane.springmeyer <dane.springmeyer@dbea3d40-3153-0410-b551-57805770c6e6>
2008-09-05 07:40:10 +0000

    Another try to test matching mapnik output from mapserver
    git-svn-id: http://mapnik-utils.googlecode.com/svn@140 dbea3d40-3153-0410-b551-57805770c6e6

--- trunk/tutorials/hello_world/mapserver_comparison/mapfile_config/shp2img.sh
+++ trunk/tutorials/hello_world/mapserver_comparison/mapfile_config/shp2img.sh
@@ -1,2 +1,3 @@
 shp2img -m world.map -l world -o map/hello_world_with_mapserver_mapfile.png
 shp2img -m world.map -l world_opacity -o map/hello_world_with_mapserver_mapfile_opacity.png
+shp2img -m world.map -l thomas -o map/hello_world_with_mapserver_mapfile_thomas.png
