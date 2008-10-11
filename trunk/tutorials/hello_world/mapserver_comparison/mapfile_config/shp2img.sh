ee66441e92a56ace28657b4e8ed917d6702a5e55
dane.springmeyer <dane.springmeyer@dbea3d40-3153-0410-b551-57805770c6e6>
2008-10-11 00:12:03 +0000

    New images rendered based on the ablity to set fractional widths in mapserver trunk
    git-svn-id: http://mapnik-utils.googlecode.com/svn@215 dbea3d40-3153-0410-b551-57805770c6e6

--- trunk/tutorials/hello_world/mapserver_comparison/mapfile_config/shp2img.sh
+++ trunk/tutorials/hello_world/mapserver_comparison/mapfile_config/shp2img.sh
@@ -1,3 +1,7 @@
 shp2img -m world.map -l world -o map/hello_world_with_mapserver_mapfile.png
 shp2img -m world.map -l world_opacity -o map/hello_world_with_mapserver_mapfile_opacity.png
 shp2img -m world.map -l thomas -o map/hello_world_with_mapserver_mapfile_thomas.png
+
+shp2img -m world_doubles.map -l world -o map/hello_world_with_mapserver_mapfile_r7971.png
+shp2img -m world_doubles.map -l world_opacity -o map/hello_world_with_mapserver_mapfile_opacity_r7971.png
+shp2img -m world_doubles.map -l thomas -o map/hello_world_with_mapserver_mapfile_thomas_r7971.png
