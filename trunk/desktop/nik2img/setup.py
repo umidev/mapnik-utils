6626011b03118de37738ec89b9b5c6087e146b21
dane.springmeyer <dane.springmeyer@dbea3d40-3153-0410-b551-57805770c6e6>
2008-11-09 22:33:08 +0000

    Making the long_description dynamic and use Restructured Text
    git-svn-id: http://mapnik-utils.googlecode.com/svn@311 dbea3d40-3153-0410-b551-57805770c6e6

--- trunk/desktop/nik2img/setup.py
+++ trunk/desktop/nik2img/setup.py
@@ -5,18 +5,20 @@ from distutils.core import setup
 # Dynamically fetch the version and licence
 version = __import__('nik2img').__version__
 license = __import__('nik2img').__license__
+readme = file('README.txt','rb').read()
 
 setup(name='nik2img',
         version = version,
         py_modules=['nik2img'],
         #py_modules = ['cascadenik.compile', 'cascadenik.style']
         description='A mapfile to image converter for Mapnik',
-        long_description='Use nik2img to interact with the Mapnik C++/Python mapping toolkit. Mirrors the utility of the shp2img script provided with the MapServer project and complements the `generate_image.py` and `generate_tiles.py` scripts used by the OpenStreetMap project to render Mapnik graphics.',
+        long_description=readme,
         author='Dane Springmeyer',
         author_email='dbsgeo@gmail.com',
         platforms='OS Independent',
         license=license,
         requires=['Mapnik'],
+        #test_suite = 'tests.run_doc_tests',
         keywords='Mapnik,gis,geospatial',
         url='http://mapnik-utils.googlecode.com/',
         classifiers=['Development Status :: 5 - Production/Stable',
@@ -24,6 +26,7 @@ setup(name='nik2img',
                      'Environment :: Web Environment',
                      'Intended Audience :: End Users/Desktop',
                      'Intended Audience :: Developers',
+                     'Intended Audience :: Science/Research',
                      'Operating System :: OS Independent',
                      'Programming Language :: Python',
                      'Topic :: Utilities'],
