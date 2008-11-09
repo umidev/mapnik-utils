0e562ea9db73ade5ed4908e1e5eabdfcf0bd9d21
dane.springmeyer <dane.springmeyer@dbea3d40-3153-0410-b551-57805770c6e6>
2008-11-09 23:21:15 +0000

    little monkey deploy script to tag a new release and upload to pypi
    git-svn-id: http://mapnik-utils.googlecode.com/svn@314 dbea3d40-3153-0410-b551-57805770c6e6

---
+++ trunk/desktop/nik2img/deploy.py
@@ -0,0 +1,21 @@
+#!/usr/bin/env python
+
+import os
+version = __import__('nik2img').__version__
+
+tag_dir= '../../../tags/nik2img/'
+
+os.system('sudo rm dist/ build/ -r -f')
+print
+os.system('rm *.pyc')
+print
+os.system('svn cp ../nik2img/ %s/%s' % (tag_dir,version))
+print
+os.system('python setup.py sdist upload')
+print
+os.system('cp dist/nik2img-%s.tar.gz %s' % (version,tag_dir))
+print
+os.system('svn add %s/nik2img-%s.tar.gz' % (tag_dir,version))
+print
+os.system('sudo rm dist/ build/ -r -f')
+
