f11af844415ab62de9e8743358b4aa9ca2bcc478
dane.springmeyer <dane.springmeyer@dbea3d40-3153-0410-b551-57805770c6e6>
2008-11-17 00:04:42 +0000

    Added notes about where to read about setuptools
    git-svn-id: http://mapnik-utils.googlecode.com/svn@330 dbea3d40-3153-0410-b551-57805770c6e6

--- trunk/desktop/nik2img/setup.py
+++ trunk/desktop/nik2img/setup.py
@@ -1,6 +1,15 @@
 #!/usr/bin/env python
 
-from distutils.core import setup
+# Learned it all here: http://ianbicking.org/docs/setuptools-presentation/
+# and here: http://peak.telecommunity.com/DevCenter/setuptools
+
+
+try:
+    from setuptools import setup, find_packages
+except:
+    from ez_setup import use_setuptools
+    use_setuptools()
+    from setuptools import setup, find_packages
 
 # Dynamically fetch the version, licence, and readme
 version = __import__('nik2img').__version__
@@ -18,17 +27,25 @@ setup(name='nik2img',
         platforms='OS Independent',
         license=license,
         requires=['Mapnik'],
-        #test_suite = 'tests.run_doc_tests',
+        test_suite = 'tests.run_doc_tests',
         keywords='Mapnik,gis,geospatial',
         url='http://mapnik-utils.googlecode.com/',
-        classifiers=['Development Status :: 5 - Production/Stable',
-                     'Environment :: Console',
-                     'Environment :: Web Environment',
-                     'Intended Audience :: End Users/Desktop',
-                     'Intended Audience :: Developers',
-                     'Intended Audience :: Science/Research',
-                     'Operating System :: OS Independent',
-                     'Programming Language :: Python',
-                     'Topic :: Utilities'],
+        classifiers=[
+        'Development Status :: 5 - Production/Stable',
+        'Environment :: Console',
+        'Environment :: Web Environment',
+        'Intended Audience :: End Users/Desktop',
+        'Intended Audience :: Developers',
+        'Intended Audience :: Science/Research',
+        'Operating System :: OS Independent',
+        'Programming Language :: Python',
+        'Topic :: Internet :: WWW/HTTP :: Dynamic Content',
+        'Topic :: Utilities'
+        ],
         scripts = ['nik2img.py'],
+        #packages=['niktests'],
+        packages=find_packages(exclude=['tests','data','niktests']),
+        zip_safe=False,
+        #package_dir={'niktests': 'niktests'},
+        package_data={'niktest': ['data/*']},
         )
