7e2f37f3f7913ab1e04ccd53145835bf0810d30b
dane.springmeyer <dane.springmeyer@dbea3d40-3153-0410-b551-57805770c6e6>
2008-12-01 02:02:01 +0000

    Increment nik2img version number back to 'SVN'
    git-svn-id: http://mapnik-utils.googlecode.com/svn@411 dbea3d40-3153-0410-b551-57805770c6e6

--- trunk/nik2img/nik2img.py
+++ trunk/nik2img/nik2img.py
@@ -2,7 +2,7 @@
 
 __author__ = "Dane Springmeyer (dbsgeo [ -a- ] gmail.com)"
 __copyright__ = "Copyright 2008, Dane Springmeyer"
-__version__ = "0.2.2"
+__version__ = "0.2.2SVN"
 __license__ = "GPLv2"
 
 import os
