596a24f114d5376256b4ae062c34a833097e2b8f
dane.springmeyer <dane.springmeyer@dbea3d40-3153-0410-b551-57805770c6e6>
2008-12-01 02:01:27 +0000

    Fixed svn add and rm typos
    git-svn-id: http://mapnik-utils.googlecode.com/svn@410 dbea3d40-3153-0410-b551-57805770c6e6

--- trunk/nik2img/deploy.py
+++ trunk/nik2img/deploy.py
@@ -33,9 +33,9 @@ tag_dir= '../../tags/%s/' % app
 
 call('sudo rm *.egg* *.pyc tests_output dist/ build/ -r -f')
 call('svn cp ../%s/ %s/%s' % (app,tag_dir,version))
-call('rm %s/%s/deploy.py' % (tag_dir,version))
+call('svn rm %s/%s/deploy.py' % (tag_dir,version))
 call('python setup.py sdist upload')
 call('cp dist/%s-%s.tar.gz %s' % (app,version,tag_dir))
-call('svn add %s/%s-%s.tar.gz' % (app,tag_dir,version))
+call('svn add %s/%s-%s.tar.gz' % (tag_dir,app,version))
 call('sudo rm *.egg* tests_output dist/ build/ -r -f')
 
