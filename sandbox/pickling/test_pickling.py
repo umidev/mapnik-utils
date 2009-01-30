15c0879b0599f3e39d374bed918bdbe7559cae36
dane.springmeyer <dane.springmeyer@dbea3d40-3153-0410-b551-57805770c6e6>
2009-01-30 07:12:31 +0000

    a few tests of pickling
    git-svn-id: http://mapnik-utils.googlecode.com/svn@518 dbea3d40-3153-0410-b551-57805770c6e6

---
+++ sandbox/pickling/test_pickling.py
@@ -0,0 +1,91 @@
+import pickle
+from mapnik import *
+from StringIO import StringIO
+from copy import copy
+
+# Have pickling
+# Map, Envelope, Paramaters, Color
+# Remain:
+'''
+'Coord','Datasource','Feature', 'Featureset', 'Filter','Layer','Query','Rule','Style', 'Symbolizer', 'Symbolizers'
+'''
+
+# 0 is ASCII, 1 is old binary, 2 is new binary
+PROTOCOL = 2
+
+def pack_mem(obj):
+    return pickle.loads(pickle.dumps(obj,protocol=PROTOCOL))
+
+def pack_file(obj):
+    file_out = open('obj.pkl', 'wb')
+    pickle.dump(obj,file_out,protocol=PROTOCOL)
+    file_out.close()
+    file_in = pickle.load(open('obj.pkl', 'rb'))
+    return file_in
+
+def pack_stream(obj):
+    stream_out = StringIO()
+    pickle.dump(m,stream_out,protocol=PROTOCOL)
+    stream_out.flush()
+    stream_in = StringIO(stream_out.getvalue())
+    stream = pickle.loads(stream_in.getvalue())
+    return stream
+
+def pack_em(obj):
+    packed = pack_mem(obj),pack_file(obj),pack_stream(obj)
+    return list(packed)
+
+def test_equivalence(pickles):
+    failed = False
+    while pickles:
+        for one_pickle in pickles:
+            maps = [p for p in pickles if isinstance(p,Map)]
+            for other in maps:
+                if isinstance(one_pickle,Map) and not one_pickle.__getstate__() ==  other.__getstate__():
+                  print '%s and %s objects do not appear equivalent...' % (one_pickle,other)
+                  failed = True
+            non_maps = [p for p in pickles if not isinstance(p,Map)]
+            for other in non_maps:
+                if not isinstance(one_pickle,Map) and not one_pickle.__getinitargs__() ==  other.__getinitargs__():
+                  print '%s and %s objects do not appear equivalent...' % (one_pickle,other)
+                  failed = True
+
+        pickles.remove(one_pickle)
+        if not failed:
+            print 'Object is functionally equivalent to others'
+
+#
+## mapnik::Map
+#
+
+maps = []
+m = Map(45,45)
+m.background = Color(0,0,0)
+m.buffer_size = 10
+
+maps.extend(pack_em(m))
+
+#
+## mapnik::color
+#
+
+colors = []
+obj = Color('steelblue')
+obj.a = 0
+colors.append(pickle.loads(pickle.dumps(obj,protocol=PROTOCOL)))
+
+obj = Color(70,130,180,0)
+colors.append(pickle.loads(pickle.dumps(obj,protocol=PROTOCOL)))
+
+
+coords = []
+coord = Coord(-122,48)
+#coords.append(pack_em(coord))
+
+def main():
+    global tests
+    test_equivalence(maps)
+    test_equivalence(colors)
+
+if __name__ == '__main__':
+    main()
