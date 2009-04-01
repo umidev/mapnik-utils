84ebcd8787df8817c379764db291ae85d7e1bc8a
dane.springmeyer <dane.springmeyer@dbea3d40-3153-0410-b551-57805770c6e6>
2009-04-01 00:17:33 +0000

    tests for get feature info with population data
    git-svn-id: http://mapnik-utils.googlecode.com/svn@610 dbea3d40-3153-0410-b551-57805770c6e6

--- example_code/wms/map_factory.py
+++ example_code/wms/map_factory.py
@@ -12,18 +12,17 @@ PROJ4_STRING = '+init=epsg:4326'
 # http://localhost/cgi-bin/mapnikwms.py?LAYERS=world&SERVICE=WMS&VERSION=1.1.1&REQUEST=GetMap&STYLES=&EXCEPTIONS=application%2Fvnd.ogc.se_inimage&FORMAT=image%2Fpng&SRS=EPSG%3A3395&BBOX=-20037400.000000,-19929239.110000,%2020037400.000000,18375854.709643&WIDTH=256&HEIGHT=256
 
 class WMSFactory(BaseWMSFactory):
-  def __init__(self):
-    BaseWMSFactory.__init__(self)
-    sty = Style()
-    rl = Rule()
-    rl.symbols.append(PolygonSymbolizer(Color('#f2eff9')))
-    rl.symbols.append(LineSymbolizer(Color('steelblue'),.1))
-    sty.rules.append( rl )
-    self.register_style('s', sty)
-    lyr = Layer('world',PROJ4_STRING)
-    lyr.title = 'World Borders'
-    lyr.abstract = 'World Test'
-    lyr.queryable = True
-    lyr.datasource = Shapefile(file=SHAPEFILE)
-    self.register_layer(lyr,'s',('s',))
-    self.finalize()
+    def __init__(self):
+        BaseWMSFactory.__init__(self)
+        sty,rl = Style(), Rule()
+        poly = PolygonSymbolizer(Color('#f2eff9'))
+        line = LineSymbolizer(Color('steelblue'),.1)
+        rl.symbols.extend([poly,line])
+        sty.rules.append(rl)
+        self.register_style('world_style',sty)
+        lyr = Layer('world',PROJ4_STRING)
+        lyr.datasource = Shapefile(file=SHAPEFILE)
+        lyr.title = 'World Borders'
+        lyr.abstract = 'Country Borders of the World'
+        self.register_layer(lyr,'world_style',('world_style',))
+        self.finalize()
--- example_code/wms/xml_factory.py
+++ example_code/wms/xml_factory.py
@@ -1,9 +1,7 @@
 from mapnik.ogcserver.WMS import BaseWMSFactory
 
-# note, this class for loading an xml map requires patch from http://trac.mapnik.org/ticket/129
-
 class WMSFactory(BaseWMSFactory):
   def __init__(self):
     BaseWMSFactory.__init__(self)
-    self.loadXML('/Users/spring/mapnik/osm.xml')
+    self.loadXML('population.xml')
     self.finalize()
