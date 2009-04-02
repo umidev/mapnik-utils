423394259832a59a5b2968333de19981f3a53d7b
dane.springmeyer <dane.springmeyer@dbea3d40-3153-0410-b551-57805770c6e6>
2009-04-02 16:15:21 +0000

    bump svn version to 0.3.0SVN
    git-svn-id: http://mapnik-utils.googlecode.com/svn@621 dbea3d40-3153-0410-b551-57805770c6e6

--- trunk/nik2img/nik2img.py
+++ trunk/nik2img/nik2img.py
@@ -2,7 +2,7 @@
 
 __author__ = "Dane Springmeyer (dbsgeo [ -a- ] gmail.com)"
 __copyright__ = "Copyright 2008, Dane Springmeyer"
-__version__ = "0.2.3"
+__version__ = "0.3.0SVN"
 __license__ = "GPLv2"
 
 import os
