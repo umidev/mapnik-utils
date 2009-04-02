559063ff67ba817b8c49925e6ec353bd665f949f
dane.springmeyer <dane.springmeyer@dbea3d40-3153-0410-b551-57805770c6e6>
2009-04-02 16:11:47 +0000

    tag nik2img 0.2.3 release
    git-svn-id: http://mapnik-utils.googlecode.com/svn@620 dbea3d40-3153-0410-b551-57805770c6e6

---
+++ tags/nik2img/0.2.3/setup.py
@@ -0,0 +1,31 @@
+from distutils.core import setup
+
+version = '0.2.3'
+app = 'nik2img'
+description = 'A mapfile to image generator for the Mapnik C++/Python mapping toolkit'
+url = 'http://mapnik-utils.googlecode.com/'
+readme = file('README.txt','rb').read()
+
+setup(name='%s' % app,
+      version=version,
+      description=description,
+      long_description=readme,
+      author='Dane Springmeyer',
+      author_email='dbsgeo@gmail.com',
+      requires=['Mapnik'],
+      keywords='mapnik,gis,geospatial,shp2img',
+      url=url,
+      py_modules=['%s' % app],
+      #packages=['%s' % app],
+      scripts = ['nik2img.py'],
+      classifiers=['Development Status :: 5 - Production/Stable',
+                    'Environment :: Console',
+                    'Environment :: Web Environment',
+                    'Intended Audience :: End Users/Desktop',
+                    'Intended Audience :: Developers',
+                    'Intended Audience :: Science/Research',
+                    'Operating System :: OS Independent',
+                    'Programming Language :: Python',
+                    'Topic :: Internet :: WWW/HTTP :: Dynamic Content',
+                    'Topic :: Utilities'],
+      )
