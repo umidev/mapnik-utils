494c1b572fef3c2daca9ccf56c3f798975c10f2a
dane.springmeyer <dane.springmeyer@dbea3d40-3153-0410-b551-57805770c6e6>
2009-04-03 19:39:01 +0000

    touchup imports
    git-svn-id: http://mapnik-utils.googlecode.com/svn@626 dbea3d40-3153-0410-b551-57805770c6e6

--- example_code/wms/localogc.py
+++ example_code/wms/localogc.py
@@ -1,16 +1,15 @@
 #!/usr/bin/env python
 
 import sys
-#sys.path.append('/Users/spring/projects/mapnik-dev/trunk/bindings/python/')
 from mapnik.ogcserver.wsgi import WSGIApp
-from wsgiref.simple_server import make_server
 
 # add to the PYTHONPATH the folder that contains map_factory.py (and ogcserver.conf)
 sys.path.append('/Users/spring/projects/utils/example_code/wms/')
-sys.path.append('/Users/spring/projects/mapnik-dev/trunk/bindings/python/')
+#sys.path.append('/Users/spring/projects/mapnik-dev/trunk/bindings/python/')
 application = WSGIApp('/Users/spring/projects/utils/example_code/wms/ogcserver.conf')
 
 if __name__ == '__main__':
+    from wsgiref.simple_server import make_server
     httpd = make_server('localhost', 8000, application)
     print "Listening on port 8000...."
     httpd.serve_forever()
