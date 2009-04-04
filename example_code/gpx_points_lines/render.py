152d7ed41cd776efe95e9d10bd2a8267e0ca5a7e
dane.springmeyer <dane.springmeyer@dbea3d40-3153-0410-b551-57805770c6e6>
2009-04-04 20:16:56 +0000

    test for ogr input
    git-svn-id: http://mapnik-utils.googlecode.com/svn@628 dbea3d40-3153-0410-b551-57805770c6e6

---
+++ example_code/gpx_points_lines/render.py
@@ -0,0 +1,25 @@
+#!/usr/bin/env python
+
+from mapnik import *
+
+m = Map(600,300,'+proj=latlong +datum=WGS84')
+
+s,r = Style(), Rule()
+r.symbols.append(PolygonSymbolizer(Color('#f2eff9')))
+r.symbols.append(LineSymbolizer(Color('rgb(50%,50%,50%)'),0.1))
+s.rules.append(r)
+m.append_style('My Style',s)
+lyr = Layer('world')
+lyr.datasource = Ogr(file='test_point_line.gpx',layer='routes')
+
+# tracks
+# routes
+# waypoints
+# route_points
+# track_points
+
+lyr.styles.append('My Style')
+m.layers.append(lyr)
+
+m.zoom_to_box(lyr.envelope())
+render_to_file(m, 'gps_tracks.png')
