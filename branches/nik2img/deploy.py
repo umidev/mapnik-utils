b6d14dd71033b7c0630b432516ca6033b356f3e5
dane.springmeyer <dane.springmeyer@dbea3d40-3153-0410-b551-57805770c6e6>
2009-05-01 04:51:00 +0000

    touch up deploy script
    git-svn-id: http://mapnik-utils.googlecode.com/svn@692 dbea3d40-3153-0410-b551-57805770c6e6

--- branches/nik2img/deploy.py
+++ branches/nik2img/deploy.py
@@ -5,17 +5,19 @@ Release Steps
 -------------
  * create ~./pypirc file with pypi user:pass
  * Edit CHANGELOG.txt
- * Temporarily remove SVN tag from __version__
+ * Increment '__version__' in main script and 'version' in setup.py
  * Run `deploy.py` to create sdist, upload, and create tag
- * commit tag
+ * Commit tag
  * Update Google Code wiki
 """
-
-app = 'nik2img'
 
 import sys
 import time
 from subprocess import call as subcall
+
+app = 'nik2img'
+version = __import__(app).__version__
+tag_dir= '../../tags/%s/' % app
 
 def call(cmd):
   try:
@@ -27,15 +29,23 @@ def call(cmd):
   except OSError, E:
     sys.exit(E)
 
-version = __import__(app).__version__
+def cleanup():
+    call('sudo rm *.egg* *.pyc tests_output dist/ build/ -r -f')
 
-tag_dir= '../../tags/%s/' % app
+def tag():
+    call('svn cp ../%s/ %s/%s' % (app,tag_dir,version))
+    call('svn rm %s/%s/deploy.py' % (tag_dir,version))
 
-call('sudo rm *.egg* *.pyc tests_output dist/ build/ -r -f')
-call('svn cp ../%s/ %s/%s' % (app,tag_dir,version))
-call('svn rm %s/%s/deploy.py' % (tag_dir,version))
-call('python setup.py sdist upload')
-call('cp dist/%s-%s.tar.gz %s' % (app,version,tag_dir))
-call('svn add %s/%s-%s.tar.gz' % (tag_dir,app,version))
-call('sudo rm *.egg* tests_output dist/ build/ -r -f')
+def package():
+    call('python setup.py sdist upload')
+    call('cp dist/%s-%s.tar.gz %s' % (app,version,tag_dir))
+    call('svn add %s/%s-%s.tar.gz' % (tag_dir,app,version))
 
+def main():
+    cleanup()
+    tag()
+    package()
+    cleanup()
+    
+if __name__ = '__main__':
+    main()
