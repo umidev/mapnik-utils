c6c8bffe5f660259332d91679379cd3a04eb1ab8
dane.springmeyer <dane.springmeyer@dbea3d40-3153-0410-b551-57805770c6e6>
2009-05-01 23:26:24 +0000

    turn into a CairoMap class for drawing title and neatline
    git-svn-id: http://mapnik-utils.googlecode.com/svn@705 dbea3d40-3153-0410-b551-57805770c6e6

--- sandbox/cairo_contexts/render_to_context.py
+++ sandbox/cairo_contexts/render_to_context.py
@@ -1,61 +1,78 @@
 #!/usr/bin/env python
 
 import cairo
-from mapnik import *
+import mapnik
+import platform
 
-format = 'svg'
-filename = 'world.%s' % format
-
-m = Map(600,400,'+proj=latlong +datum=WGS84')
-load_map(m,'../../example_code/world_population/population.xml')
-
-# remove text otherwise bus errors in libcairo.dylib...
-#m.remove_style('countries_label')
-m.zoom_all()
-m.zoom(.98)
-
-formats = {'svg': cairo.SVGSurface,
-                       'pdf':cairo.PDFSurface,
-                       'ps':cairo.PSSurface,
-                       }
-
-file_output = open(filename, 'w')
-surface = formats[format](file_output.name, m.width, m.height)
-ctx = cairo.Context(surface)
-
-render(m, ctx,10,10)
-
+surfaces = {'svg': cairo.SVGSurface,
+           'pdf':cairo.PDFSurface,
+           'ps':cairo.PSSurface,
+           }
+           
 def cairo_color(c):
-     ctx_c = (c.r/255.0,c.g/255.0,c.b/255.0,c.a/255.0)
-     return ctx_c
+    """ Return a Cairo color tuple from a Mapnik Color."""
+    ctx_c = (c.r/255.0,c.g/255.0,c.b/255.0,c.a/255.0)
+    return ctx_c
+
+class CairoMap(object):
+    def __init__(self,mapnik_map,map_output):
+        self.m = mapnik_map
+        self.map_output = map_output
+        self.surface = self.create_surface()
+
+    def draw_title(self,text,size=10,color=mapnik.Color('black')):
+        """ Draw a Map Title near the top of a page."""
+        middle = self.m.width/2.0
+        ctx = cairo.Context(self.surface)
+        ctx.set_source_rgba(*cairo_color(color))
+        ctx.select_font_face("DejaVu Sans Book", cairo.FONT_SLANT_NORMAL, cairo.FONT_WEIGHT_NORMAL)
+        ctx.set_font_size(size)
+        x_bearing, y_bearing, width, height = ctx.text_extents(text)[:4]
+        ctx.move_to(middle - width / 2 - x_bearing, 20.0 - height / 2 - y_bearing)
+        ctx.show_text(text)
     
-def draw_title(m,ctx,text,size=10,color=Color('black')):
-    w,h = m.width, m.height
-    map_pts = [[0,0],[w,0],[w,h],[0,h]]
-    ctx.select_font_face("DejaVu Sans Book",
-                cairo.FONT_SLANT_NORMAL, cairo.FONT_WEIGHT_NORMAL)
-    ctx.set_font_size(size)
-    x_bearing, y_bearing, width, height = ctx.text_extents(text)[:4]
-    ctx.move_to(w/2 - width / 2 - x_bearing, 20.0 - height / 2 - y_bearing)
-    ctx.show_text(text)
+    def draw_neatline(self,width=10,color=mapnik.Color('black')):
+        ctx = cairo.Context(self.surface)
+        w,h = self.m.width, self.m.height
+        map_pts = [[0,0],[w,0],[w,h],[0,h]]
+        ctx.set_line_width(width)
+        ctx.set_source_rgba(*cairo_color(color))
+        ctx.move_to(*map_pts[0])
+        for pt in map_pts:
+            ctx.line_to(*pt)
+        ctx.close_path()
+        ctx.stroke()
+
+    def create_surface(self):
+        self.format = self.map_output.split('.')[1]
+        file_output = open(self.map_output, 'w')
+        surface = surfaces[self.format](file_output.name, self.m.width, self.m.height)
+        return surface
         
-def draw_neatline(m,ctx,width=10,color=Color('black')):
-    w,h = m.width, m.height
-    map_pts = [[0,0],[w,0],[w,h],[0,h]]
-    ctx.set_line_width(width)
-    ctx.set_source_rgba(*cairo_color(color))
-    ctx.move_to(*map_pts[-1])
-    for pt in map_pts:
-        ctx.line_to(*pt)
-    ctx.close_path()
-    ctx.stroke()
+    def render(self):
+        mapnik.render(m, cairo.Context(self.surface))
+    
+    def close(self):
+        self.surface.finish()
+    
 
-c = Color('darkred')
-c.a = 250
+if __name__ == '__main__':
+    m = mapnik.Map(600,400)
+    mapnik.load_map(m,'../../example_code/world_population/population.xml')
+    out = 'world.svg'
+    c_map = CairoMap(m,out)
+    # zoom map...
+    c_map.m.zoom_all()
+    c_map.m.zoom(.98)
+    # render map onto surface, leave active...
+    c_map.render()
+    # draw other items on top...
+    border = mapnik.Color('black')
+    c_map.draw_neatline(width=5,color=border)
+    text = mapnik.Color('darkred')
+    c_map.draw_title('Mapnik Map of the World',size=15,color=text)
+    c_map.close()
 
-draw_neatline(m,ctx,width=5,color=c)
-draw_title(m,ctx,'Mapnik Map of the World!',size=15)
-surface.finish()
-
-import os
-os.system('open %s' % filename)
+    if platform.system() == 'Darwin':
+        import os
+        os.system('open %s' % out)
