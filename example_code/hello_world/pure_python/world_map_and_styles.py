e5496856c91644f43deffda4f2567035cb5f91b0
dane.springmeyer <dane.springmeyer@dbea3d40-3153-0410-b551-57805770c6e6>
2009-05-04 02:30:41 +0000

    update,simplify tutorial code
    git-svn-id: http://mapnik-utils.googlecode.com/svn@715 dbea3d40-3153-0410-b551-57805770c6e6

--- example_code/hello_world/pure_python/world_map_and_styles.py
+++ example_code/hello_world/pure_python/world_map_and_styles.py
@@ -1,21 +1,26 @@
 #!/usr/bin/env python
 
 from mapnik import *
-m = Map(600,300,'+proj=latlong +datum=WGS84')
 
+# Map
+m = Map(600,300,'+proj=latlong +datum=WGS84')
 m.background = Color('steelblue')
-s = Style()
-r=Rule()
-r.symbols.append(PolygonSymbolizer(Color('#f2eff9')))
-r.symbols.append(LineSymbolizer(Color('rgb(50%,50%,50%)'),0.1))
+
+# Styles
+poly = PolygonSymbolizer(Color('#f2eff9'))
+line = LineSymbolizer(Color('rgb(50%,50%,50%)'),0.1)
+s, r = Style(), Rule()
+r.symbols.extend([poly,line])
 s.rules.append(r)
 m.append_style('My Style',s)
+
+# Layer
 lyr = Layer('world')
 lyr.datasource = Shapefile(file='../data/world_borders')
+lyr.srs = '+proj=latlong +datum=WGS84'
 lyr.styles.append('My Style')
 m.layers.append(lyr)
 
+# Render
 m.zoom_to_box(lyr.envelope())
 render_to_file(m, 'map/hello_world_in_pure_python.png')
-render_to_file(m, 'map/hello_world_in_pure_python_small.png','png256')
-save_map(m, 'map/hello_world_in_pure_python.xml')
