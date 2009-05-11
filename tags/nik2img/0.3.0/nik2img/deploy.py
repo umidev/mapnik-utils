9b8b04638536eeab2d4b73eb51861c070f4a536c
dane.springmeyer <dane.springmeyer@dbea3d40-3153-0410-b551-57805770c6e6>
2009-05-11 06:22:55 +0000

    tag 0.3.0 release
    git-svn-id: http://mapnik-utils.googlecode.com/svn@741 dbea3d40-3153-0410-b551-57805770c6e6

---
+++ tags/nik2img/0.3.0/nik2img/deploy.py
@@ -0,0 +1,52 @@
+#!/usr/bin/env python
+
+"""
+Release Steps
+-------------
+ * create ~./pypirc file with pypi user:pass
+ * Edit CHANGELOG.txt
+ * rebuild MANIFEST 'python setup.py sdist --manifest-only'
+ * Increment '__version__' in main script and 'version' in setup.py
+ * Run `deploy.py` to create sdist, upload, and create tag
+ * Commit tag
+ * Update Google Code wiki
+"""
+
+import sys
+import time
+from subprocess import call as subcall
+
+app = 'nik2img'
+version = __import__(app).__version__
+tag_dir= '../../tags/%s/' % app
+
+def call(cmd):
+  try:
+    response = subcall(cmd,shell=True)
+    print
+    time.sleep(1)
+    if response < 0:
+      sys.exit(response)
+  except OSError, E:
+    sys.exit(E)
+
+def cleanup():
+    call('sudo rm *.egg* *.pyc dist/ build/ -r -f')
+
+def tag():
+    call('svn cp ../%s/ %s/%s' % (app,tag_dir,version))
+    call('svn rm %s/%s/deploy.py' % (tag_dir,version))
+
+def package():
+    call('python setup.py sdist upload')
+    call('cp dist/%s-%s.tar.gz %s' % (app,version,tag_dir))
+    call('svn add %s/%s-%s.tar.gz' % (tag_dir,app,version))
+
+def main():
+    cleanup()
+    tag()
+    package()
+    cleanup()
+    
+if __name__ == '__main__':
+    main()
---
+++ tags/nik2img/0.3.0/tests/pymap.py
@@ -0,0 +1,4 @@
+import mapnik
+
+m = mapnik.Map(1,1)
+mapnik.load_map(m,'tests/mapfile_wgs84.xml')
