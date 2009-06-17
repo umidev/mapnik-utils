929807ccee8b3877bb4eaa26c9d7094bc2c8b4cf
dane.springmeyer <dane.springmeyer@dbea3d40-3153-0410-b551-57805770c6e6>
2009-06-17 16:37:46 +0000

    improve deploy script
    git-svn-id: http://mapnik-utils.googlecode.com/svn@785 dbea3d40-3153-0410-b551-57805770c6e6

--- trunk/nik2img/deploy.py
+++ trunk/nik2img/deploy.py
@@ -16,37 +16,45 @@ import sys
 import time
 from subprocess import call as subcall
 
+DEBUG = False
+
 app = 'nik2img'
 version = __import__(app).__version__
-tag_dir= '../../tags/%s/' % app
+tag_dir= '../../tags/%s' % app
 
 def call(cmd):
-  try:
-    response = subcall(cmd,shell=True)
-    print
-    time.sleep(1)
-    if response < 0:
-      sys.exit(response)
-  except OSError, E:
-    sys.exit(E)
+  if DEBUG:
+      print 'DEBUG: will execute -> %s' % cmd  
+  else:
+      try:
+        response = subcall(cmd,shell=True)
+        print
+        time.sleep(1)
+        if response < 0:
+          sys.exit(response)
+      except OSError, E:
+        sys.exit(E)
 
 def cleanup():
     call('sudo -rf rm *.egg* *.pyc dist/ build/')
 
 def tag():
-    call('svn cp ../%s/ %s/%s' % (app,tag_dir,version))
-    call('svn rm %s/%s/deploy.py' % (tag_dir,version))
+    call('svn cp ../%s %s/%s/' % (app,tag_dir,version))
 
 def package():
     call('python setup.py sdist upload')
     call('cp dist/%s-%s.tar.gz %s' % (app,version,tag_dir))
     call('svn add %s/%s-%s.tar.gz' % (tag_dir,app,version))
 
+def commit():
+    call('svn ci %s -m "tagging %s %s release"' % (tag_dir,app,version))
+    
 def main():
     cleanup()
     tag()
     package()
     cleanup()
+    commit()
     
 if __name__ == '__main__':
     main()
