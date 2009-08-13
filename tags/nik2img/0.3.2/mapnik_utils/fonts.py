00bf4fc9ee1c62ad1672ae8cf1256e986d216b42
dane.springmeyer <dane.springmeyer@dbea3d40-3153-0410-b551-57805770c6e6>
2009-08-13 15:24:58 +0000

    tagging nik2img 0.3.2 release
    git-svn-id: http://mapnik-utils.googlecode.com/svn@797 dbea3d40-3153-0410-b551-57805770c6e6

---
+++ tags/nik2img/0.3.2/mapnik_utils/fonts.py
@@ -0,0 +1,19 @@
+import mapnik
+
+class FontHandler(object):
+    def __init__(self):
+        self.added = []
+        self.failed = []
+        self.fontdir = mapnik.fontscollectionpath
+    
+    @property
+    def available(self):
+        return [f for f in mapnik.FontEngine.face_names()]
+
+    def add_fonts(self,fonts):
+        engine = mapnik.FontEngine.instance()
+        for font in fonts:
+            if engine.register_font(font):
+                self.added.append(font)
+            else:
+                self.failed.append(font)
