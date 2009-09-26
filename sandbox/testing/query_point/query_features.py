215807c5dc91bf962c4e29d2fc90afc999d4a638
dane.springmeyer <dane.springmeyer@dbea3d40-3153-0410-b551-57805770c6e6>
2009-09-26 22:41:24 +0000

    update path to shapefile and make example compatible across mapnik versions
    git-svn-id: http://mapnik-utils.googlecode.com/svn@826 dbea3d40-3153-0410-b551-57805770c6e6

--- sandbox/testing/query_point/query_features.py
+++ sandbox/testing/query_point/query_features.py
@@ -4,10 +4,14 @@ from mapnik import *
 m = Map(600,300,'+proj=latlong +datum=WGS84')
 
 def print_features(fs):
-    feat = fs.next()
-    while feat:
-        print feat
+    if hasattr(fs,'next'):        # mapnik 0.5.x
         feat = fs.next()
+        while feat:
+            print feat
+            feat = fs.next()
+    else:                         # mapnik 0.6.x
+        for feat in fs.features:
+            print feat
 
 m.background = Color('steelblue')
 s = Style()
@@ -17,14 +21,12 @@ r.symbols.append(LineSymbolizer(Color('rgb(50%,50%,50%)'),0.1))
 s.rules.append(r)
 m.append_style('My Style',s)
 lyr = Layer('world')
-lyr.datasource = Shapefile(file='../../data/tm_wgs84_sans_antarctica')
+lyr.datasource = Shapefile(file='../../../data/tm_wgs84_sans_antarctica')
 lyr.styles.append('My Style')
 m.layers.append(lyr)
 m.zoom_to_box(lyr.envelope())
 
-render_to_file(m, 'test.png')
-import os
-os.system('open test.png')
+#render_to_file(m, 'test.png')
 
 f_set = m.query_point(0,-122,48)
 print 'Queried Layer #%s, at longitude: %s, Latitude: %s' % (0,-122,48)
